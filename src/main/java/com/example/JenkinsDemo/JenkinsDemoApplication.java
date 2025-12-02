cbbd188c258ae46390b65a59537b1716d4cabb39
Johnny <gnaneswararaobheemuni@gmail.com>
2025-12-02 12:13:33 +0530

    commit

--- src/main/java/com/example/JenkinsDemo/JenkinsDemoApplication.java
+++ src/main/java/com/example/JenkinsDemo/JenkinsDemoApplication.java
@@ -2,6 +2,7 @@ package com.example.JenkinsDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class JenkinsDemoApplication {
@@ -9,5 +10,10 @@ public class JenkinsDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDemoApplication.class, args);
 	}
+
+    @GetMapping("/home")
+    public String hello(){
+        return "HelloWorld";
+    }
 
 }
